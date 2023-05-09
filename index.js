8376f5f877ad51ce95381504ac9235dd4192544a
ehristenko <>
2023-05-09 10:44:49 +0300

    fix

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-let ConwayGame = function (playingFieldElement) {
+let ConwayGame = function (playingFieldElement, generationElement, livingCellsElement) {
 
-    let _this = this;
+    this.playinFieldElement = playingFieldElement;
+    this.generationElement = generationElement;
+    this.livingCellsElement = livingCellsElement
 
-    this.playinFieldEllement = playingFieldElement;
+    this.playingFieldWidth = 680;
+    this.playingFieldHeight = 680;
+    this.cellWidth = 8;
+    this.cellHeight = 8;
 
-    this.playingFieldWidth = 200;
-    this.playingFieldHeight = 200;
-    this.cellWidth = 3;
-    this.cellHeight = 3;
+    this.cellColumns = null;
+    this.cellRows = null;
 
-    this.cellColumns = _this.playingFieldWidth / _this.cellWidth;
-    this.cellRows = _this.playingFieldHeight / _this.cellHeight;
+    this.stateLiveClass = "cell_live";
+    this.stateDeadClass = "cell_dead";
+    this.CellRowClassName = "cell_row";
 
-    this.stateLive = "grey";
-    this.stateDead = "white";
+    this.loopSpeed = 20;
+
+    this.curModeIdx = 0;
 
     this.cells = [];
 
-    this.maxSaveStates = 5;
-    this.loopSpeed = 50;
-    this.loopFlag = false;
-
-    this.score = 0;
+    this.generation = 0;
+    this.livingCells = 0;
 
     this.modes = {
-        random: _this.GetRandomState()
+        "random": {name: "random", state: []},
+        "clear": {name: "clear", state: []}
     };
 
-    this.curCellsState = _this.modes.random;
+    this.modesList = ["random", "clear"]
 
-    this.cellStates = [];
+    this.curCellsState = null;
 
     this.Active = false;
 
@@ -41,67 +44,80 @@ ConwayGame.prototype.Init = function () {
 
     let _this = this;
 
-    _this.playinFieldEllement.style.width = _this.playingFieldWidth + "px";
-    _this.playinFieldEllement.style.height = _this.playingFieldHeight + "px";
+    _this.cellColumns = Math.floor(_this.playingFieldWidth / _this.cellWidth);
+    _this.cellRows = Math.floor(_this.playingFieldHeight / _this.cellHeight);
+
+    _this.modes["random"].state = _this.GetRandomState();
+    _this.modes["clear"].state = _this.GetClearState();
+
+    _this.curCellsState = _this.modes[_this.modesList[_this.curModeIdx]].state
+
+    _this.RefreshCellsEl();
+
+    _this.Render();
+}
+
+ConwayGame.prototype.RefreshCellsEl = function () {
+
+    let _this = this;
 
     for (let y = 0; y < _this.cellRows; y++) {
         let cellRow = document.createElement("div");
-        _this.playinFieldEllement.appendChild(cellRow);
+        cellRow.className = _this.CellRowClassName;
+        _this.playinFieldElement.appendChild(cellRow);
         cellRow.style.display = "flex";
         let row = [];
         for (let x = 0; x < _this.cellColumns; x++) {
             let cell = document.createElement("div");
-            cell.className = "cell";
+            cell.className = _this.stateDeadClass;
             cellRow.appendChild(cell);
             cell.style.width = _this.cellWidth + "px";
             cell.style.height = _this.cellHeight + "px";
+            cell.addEventListener("mouseenter", (event) => {
+                if (event.buttons === 1) {
+                    _this.Active = false;
+                    if (cell.className === _this.stateDeadClass) {
+                        cell.className = _this.stateLiveClass;
+                        _this.curCellsState[y][x] = _this.stateLiveClass;
+                    } else {
+                        cell.className = _this.stateDeadClass;
+                        _this.curCellsState[y][x] = _this.stateDeadClass;
+                    }
+                }
+            })
+            cell.addEventListener("click", () => {
+                _this.Active = false;
+                if (cell.className === _this.stateDeadClass) {
+                    cell.className = _this.stateLiveClass;
+                    _this.curCellsState[y][x] = _this.stateLiveClass;
+                } else {
+                    cell.className = _this.stateDeadClass;
+                    _this.curCellsState[y][x] = _this.stateDeadClass;
+                }
+            })
             row.push(cell);
         }
         _this.cells.push(row);
     }
-    _this.Render();
-
-}
-
-ConwayGame.prototype.GetRandomState = function () {
-
-    let _this = this;
-    let resultCellsState = [];
-
-    for (let y = 0; y < _this.cellRows; y++) {
-        let rowStateCells = []
-        for (let x = 0; x < _this.cellColumns; x++) {
-            let state = Math.random() < 0.5 ? _this.stateLive : _this.stateDead;
-            rowStateCells.push(state);
-        }
-        resultCellsState.push(rowStateCells);
-    }
-    return resultCellsState;
 }
 
 ConwayGame.prototype.Render = function () {
 
     let _this = this;
+
     for (let y = 0; y < _this.cellRows; y++) {
         for (let x = 0; x < _this.cellColumns; x++) {
-            _this.cells[y][x].style.backgroundColor = _this.curCellsState[y][x];
+            _this.cells[y][x].className = _this.curCellsState[y][x];
         }
     }
+
+    _this.CountLivingCells();
+
+    _this.generationElement.textContent = _this.generation;
+    _this.livingCellsElement.textContent = _this.livingCells;
 }
 
-ConwayGame.prototype.Reset = function () {
-
-    let _this = this;
-
-    // if (_this.Active) {
-    //
-    // }
-    _this.score = 0;
-    _this.curCellsState = _this.GetRandomState();
-    _this.Render();
-}
-
-ConwayGame.prototype.Start = async function () {
+ConwayGame.prototype.Play = async function () {
 
     let _this = this;
 
@@ -109,12 +125,70 @@ ConwayGame.prototype.Start = async function () {
 
     while (_this.Active) {
         _this.Render()
-        _this.cellStates.push(_this.curCellsState);
         _this.GetNextState();
-        _this.score += 1;
-        console.log(_this.score);
+        _this.generation += 1;
         await new Promise(resolve => setTimeout(resolve, _this.loopSpeed));
     }
+}
+
+ConwayGame.prototype.Stop = function () {
+
+    let _this = this;
+
+    _this.Active = false;
+}
+
+ConwayGame.prototype.Reset = function () {
+
+    let _this = this;
+
+    _this.generation = 0;
+
+    if (!_this.Active) {
+        if (_this.modes[_this.modesList[_this.curModeIdx]].name === "random") {
+            _this.modes[_this.modesList[_this.curModeIdx]].state = _this.GetRandomState();
+        }
+        if (_this.modes[_this.modesList[_this.curModeIdx]].name === "clear") {
+            _this.modes[_this.modesList[_this.curModeIdx]].state = _this.GetClearState();
+        }
+    }
+    if (_this.Active) {
+        _this.Active = false;
+    }
+
+    _this.curCellsState = _this.modes[_this.modesList[_this.curModeIdx]].state
+    _this.Render();
+}
+
+ConwayGame.prototype.GetClearState = function () {
+
+    let _this = this;
+    let resultCellsState = [];
+    for (let y = 0; y < _this.cellRows; y++) {
+        let rowStateCells = []
+        for (let x = 0; x < _this.cellColumns; x++) {
+            rowStateCells.push(_this.stateDeadClass);
+        }
+        resultCellsState.push(rowStateCells);
+    }
+    return resultCellsState;
+}
+
+ConwayGame.prototype.GetRandomState = function () {
+
+    let _this = this;
+
+    let resultCellsState = [];
+
+    for (let y = 0; y < _this.cellRows; y++) {
+        let rowStateCells = []
+        for (let x = 0; x < _this.cellColumns; x++) {
+            let state = Math.random() < 0.5 ? _this.stateLiveClass : _this.stateDeadClass;
+            rowStateCells.push(state);
+        }
+        resultCellsState.push(rowStateCells);
+    }
+    return resultCellsState;
 }
 
 ConwayGame.prototype.GetNextState = function () {
@@ -127,10 +201,10 @@ ConwayGame.prototype.GetNextState = function () {
         let row = []
         for (let x = 0; x < _this.cellColumns; x++) {
             let neighbors = _this.countNeighbors(x, y);
-            if (_this.curCellsState[y][x] === _this.stateLive && (neighbors < 2 || neighbors > 3)) {
-                row.push(_this.stateDead);
-            } else if ((_this.curCellsState[y][x] === _this.stateDead) && (neighbors === 3)) {
-                row.push(_this.stateLive);
+            if (_this.curCellsState[y][x] === _this.stateLiveClass && (neighbors < 2 || neighbors > 3)) {
+                row.push(_this.stateDeadClass);
+            } else if ((_this.curCellsState[y][x] === _this.stateDeadClass) && (neighbors === 3)) {
+                row.push(_this.stateLiveClass);
             } else {
                 row.push(_this.curCellsState[y][x]);
             }
@@ -153,8 +227,8 @@ ConwayGame.prototype.countNeighbors = function(x, y) {
             const nx = x + dx;
             const ny = y + dy;
 
-            if (_this.isCoordCorrect(nx, ny)) {
-                if (_this.curCellsState[ny][nx] === _this.stateLive) {
+            if (_this.IsCoordCorrect(nx, ny)) {
+                if (_this.curCellsState[ny][nx] === _this.stateLiveClass) {
                     neighbors += 1;
                 }
             }
@@ -163,11 +237,83 @@ ConwayGame.prototype.countNeighbors = function(x, y) {
     return neighbors;
 }
 
-ConwayGame.prototype.isCoordCorrect = function(x, y) {
+ConwayGame.prototype.IsCoordCorrect = function(x, y) {
     let _this = this;
     return ((x >= 0 && y < _this.cellColumns) && (y >= 0 && y < _this.cellRows));
 }
 
+ConwayGame.prototype.ChangeMode = function (button_mode) {
+
+    let _this = this;
+
+    _this.Active = false;
+
+    if (button_mode === "next") {
+        _this.curModeIdx += 1;
+        if (_this.curModeIdx >= _this.modesList.length) {
+            _this.curModeIdx = 0;
+        }
+    } else {
+        _this.curModeIdx -= 1;
+        if (_this.curModeIdx < 0) {
+            _this.curModeIdx = _this.modesList.length - 1;
+        }
+    }
+
+    _this.curCellsState = _this.modes[_this.modesList[_this.curModeIdx]].state;
+
+    _this.Render();
+
+}
+
+ConwayGame.prototype.CountLivingCells = function () {
+
+    let _this = this;
+    _this.livingCells = 0;
+    for (let y = 0; y < _this.cellRows; y++) {
+        for (let x = 0; x < _this.cellColumns; x++) {
+            if (_this.cells[y][x].className === _this.stateLiveClass) {
+                _this.livingCells += 1;
+            }
+        }
+    }
+}
+
+ConwayGame.prototype.SetResolution = function(value) {
+
+    let _this = this;
+
+    _this.Active = false;
+
+    _this.cellWidth = _this.cellHeight = value;
+
+    _this.FullReset();
+}
+
+ConwayGame.prototype.SetSpeed = function (value) {
+
+    let _this = this;
+
+    _this.loopSpeed = value;
+    console.log(_this.loopSpeed);
+}
+
+ConwayGame.prototype.FullReset = function () {
+
+    let _this = this;
+    console.log(_this.cell);
+    let cellRows = document.querySelectorAll("." + _this.CellRowClassName)
+
+    cellRows.forEach(el => el.remove());
+
+    _this.generation = 0;
+
+    _this.cells = [];
+
+    _this.curCellsState = [];
+
+    _this.Init();
+}
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -177,91 +323,55 @@ document.addEventListener("DOMContentLoaded", function () {
     pageBody.style.height = wHeight + "px";
     pageBody.style.width = wWidth + "px";
 
-    let conwayGame = new ConwayGame(document.getElementById("playing_field"));
+    let conwayGame = new ConwayGame(
+        document.getElementById("playing_field"),
+        document.getElementById("generation"),
+        document.getElementById("living_cells")
+    );
 
-    let startButtom = document.getElementById("start");
-    let stopButtom = document.getElementById("stop")
-    let resetButtom = document.getElementById("reset")
+    let playButton = document.getElementById("play");
+    let stopButton = document.getElementById("stop")
+    let resetButton = document.getElementById("reset")
 
-    startButtom.addEventListener("click", () => {
-        console.log("start");
-        conwayGame.Start();
+    playButton.addEventListener("click", () => {
+        conwayGame.Play();
     });
 
-    resetButtom.addEventListener("click", () => {
-        console.log("reset");
+    stopButton.addEventListener("click", () => {
+        conwayGame.Stop();
+    })
+
+    resetButton.addEventListener("click", () => {
         conwayGame.Reset();
     });
+
+    let buttonNextMode = document.getElementById("mode_next")
+    let buttonPreviousMode = document.getElementById("mode_previous")
+    let showMode = document.getElementById("mode")
+
+    showMode.textContent = conwayGame.modes[conwayGame.modesList[conwayGame.curModeIdx]].name;
+
+    buttonNextMode.addEventListener("click", () => {
+        conwayGame.ChangeMode("next");
+        showMode.textContent = conwayGame.modes[conwayGame.modesList[conwayGame.curModeIdx]].name;
+    })
+    buttonPreviousMode.addEventListener("click", () => {
+        conwayGame.ChangeMode("previous");
+        showMode.textContent = conwayGame.modes[conwayGame.modesList[conwayGame.curModeIdx]].name;
+    })
+
+    let resolutionInput = document.getElementById("resolution");
+    resolutionInput.addEventListener("input", () => {
+        let resolutionValue = resolutionInput.value;
+        conwayGame.SetResolution(resolutionValue);
+    })
+
+    let speedInput = document.getElementById("speed");
+    speedInput.addEventListener("input", () => {
+        let speedValue = speedInput.value;
+        conwayGame.SetSpeed(speedValue );
+    })
 })
-
-
-
-
-
-
-
-function isStateLoop (arr) {
-    if (arr.length > maxSaveStates) {
-        arr.splice(0, 1);
-    }
-    if (arr.length < maxSaveStates) {
-        return false;
-    }
-    let firstString = arr[0];
-    console.log(arr.length)
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] === firstString) {
-            // console.log(arr[i])
-            return true;
-        }
-    }
-    return false;
-}
-
-
-
-    // playingField.style.width = playingFieldWidth + "px";
-    // playingField.style.height = playingFieldHeight + "px";
-    // playingField.style.backgroundColor = "grey";
-
-    // let startButtom = document.getElementById("start");
-    // let stopButtom = document.getElementById("stop")
-    // let resetButtom = document.getElementById("reset")
-
-    // let cells = [];
-    //
-    // for (let y = 0; y < cellRows; y++) {
-    //     let cellRow = document.createElement("div");
-    //     playingField.appendChild(cellRow);
-    //     cellRow.style.display = "flex";
-    //     let row = [];
-    //     for (let x = 0; x < cellColumns; x++) {
-    //         let cell = document.createElement("div");
-    //         cell.className = "cell";
-    //         cellRow.appendChild(cell);
-    //         cell.style.width = cellWidth + "px";
-    //         cell.style.height = cellHeight + "px";
-    //         row.push(cell);
-    //     }
-    //     cells.push(row);
-    // }
-    //
-    // var curStateCells = getRandomCellsState();
-    //
-    // renderCells(cells, curStateCells);
-    //
-    // startButtom.addEventListener("click", async function() {
-    //     console.log("start");
-    //     // console.log(cells);
-    //     // console.log(curStateCells);
-    //     // let loopFlag = true;
-    //     while (loopFlag) {
-    //     renderCells(cells, curStateCells);
-    //     curStateCells = getNextStateCells(curStateCells);
-    //     await new Promise(resolve => setTimeout(resolve, loopSpeed));
-    //     }
-    // })
-// })
 
 
 
